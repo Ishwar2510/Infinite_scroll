7df80d5b226a439bc93cb1f8c537ff8574eae933
Ishwar2510 <ishwar2510@gmail.com>
2023-02-08 13:43:13 +0530

    scrollEffectadded

--- src/App.js
+++ src/App.js
@@ -13,8 +13,32 @@ function App() {
     return state.limitReducer.page
   })
   useEffect(()=>{
-    dispatcher(dataAction(page))
+    if(page!=1){
+      dispatcher(dataAction(page))
+    }
+    
   },[page])
+
+  function handleScroll(){
+    let height = document.documentElement.scrollHeight;
+    let scrollTop = document.documentElement.scrollTop;
+    let viewHeight = window.innerHeight;
+    // console.log("scroll Height--->",height)
+    // console.log("scrollTop--->",scrollTop)
+    // console.log("window Height--->",viewHeight)
+
+    if(viewHeight+scrollTop+1 >= height){
+      // console.log("event called")
+      dispatcher({type:'incr'})
+    }
+
+  }
+  useEffect(()=>{
+    window.addEventListener('scroll',handleScroll)
+    return function (){
+      window.removeEventListener('scroll',handleScroll)
+    }
+  })
 
   return (
     <>
